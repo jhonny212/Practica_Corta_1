a2b5958cfe8c22c9c47c3ce9164cf0afcdb99d09
jhonny <jhonnygarcia201830454@cunoc.edu.gt>
2019-07-31 09:10:27 -0500

    a

--- Biblioteca/src/Bibliotecario/VisualizarEstudiantes.java
+++ Biblioteca/src/Bibliotecario/VisualizarEstudiantes.java
@@ -27,41 +27,7 @@ public class VisualizarEstudiantes extends javax.swing.JPanel {
         initComponents();
                
                 
-           
-        try {
-            FileReader fr=new FileReader("C:\\home\\alejandrobr\\NetBeansProjects\\Practica_Corta_1\\Biblioteca\\Estudiantes.txt");
-            BufferedReader br=new BufferedReader(fr);
-           
-           
-            String linea;
-           LinkedList<String> Datos = new LinkedList<String>();
-           while((linea=br.readLine())!=null){
-            Datos.add(linea);
-            }
-         for(int i=0;i<Datos.size();i++){
-             if(Datos.get(i).equals("LIBRO")){
-                 int tmp=i+1;
-                 String tmp2="";
-                
-                tmp2=Datos.get(tmp);
-                String tmp3= tmp2.replaceAll("TITULO:","");
-                      
-                      System.out.println(tmp3);
-                      
-             }
-     
-         }
-        } catch (FileNotFoundException ex) {
-<<<<<<< HEAD
-        
-        } catch (IOException ex) {
-            Logger.getLogger(VisualizarEstudiantes.class.getName()).log(Level.SEVERE, null, ex);
-=======
-             
->>>>>>> origin/Alejandro2
-        }
-    
-
+       
     }
     
 
